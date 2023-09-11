0fb94430165f20aec4785bcdf57158c8b7c216b1
Tanvi  Bisht <tanvibisht@Tanvis-MacBook-Pro.local>
2023-09-11 12:22:37 -0400

    first working multiples file

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int cntr = 0;
+        for (int i = 0; i <= 1000; i += 1) {
+            if (i % 3 == 0 | i % 5 == 0) {
+                cntr++;
+            }
+        }
+        System.out.println("There are " + cntr + " multiples of 3 and 5 under 1000.");
+    }
 }
