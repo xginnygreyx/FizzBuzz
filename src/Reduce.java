3a42ea945d8af755c6bd7ea60cb68a65f6f236eb
Tanvi  Bisht <tanvibisht@Tanvis-MacBook-Pro.local>
2023-09-11 12:30:25 -0400

    working

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int cntr = 0;
+        do{
+            if (n%2 == 0)
+            {
+                n = n/2;
+                cntr++;
+            }
+            else {
+                n = n -1;
+                cntr++;
+            }
+
+        }while (n>0);
+        System.out.println("It took us " +cntr+ " steps to get from 100 to 0.");
+    }
 }
